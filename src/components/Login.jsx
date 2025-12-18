1f379eb678aa9aa6003c4c75a1b57be8125013df
ivanroqueceb <ivan.roque.ceb@immune.institute>
2025-12-18 16:25:13 +0100

    log in

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "../css/Login.css";
+const Login = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+
+      <form>
+        <h3>Login</h3>
+
+        <label htmlFor="username">Nombre de usuario</label>
+        <input
+          type="text"
+          placeholder="Email or Telefono"
+          id="username"
+        />
+
+        <label htmlFor="password">Contraseña</label>
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+        />
+
+        <button type="submit">Log In</button>
+
+        <p className="social-text">
+        </p>
+
+        <div className="social-icons">
+          <button className="social-icon fb" type="button">
+            <i className="fa-brands fa-facebook"></i>
+          </button>
+
+          <button className="social-icon tw" type="button">
+            <i className="fa-brands fa-twitter"></i>
+          </button>
+
+          <button className="social-icon in" type="button">
+            <i className="fa-brands fa-instagram"></i>
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default Login;
