db19199a9c3e23dfec6bab87ccf3d2f126b06882
ivanroqueceb <ivan.roque.ceb@immune.institute>
2025-12-18 17:17:26 +0100

    Cambios log in

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import coches from "../coches.json"
 import Header from "./Header"
 import { Route, Routes } from "react-router-dom"
 import Login from "./Login"
+import Filtro from "./Filtro"
 
 function App() {
 
@@ -11,29 +12,9 @@ function App() {
     <Header />
     <Routes>
       <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Filtro/>}/>
     </Routes>
-      <label for="inputTipo">Tipo: </label>
-      <select name="" id="inputTipo">
-        <option value="">Nada</option>
-        <option value="SUV">SUV</option>
-        <option value="Berlina">Berlina</option>
-        <option value="Tourin">Touring</option>
-        <option value="Gran Coupé">Gran Coupé</option>
-        <option value="Compacto">Compacto</option>
-        <option value="Monovolumen">Monovolumen</option>
-        <option value="Cabrio">Cabrioo</option>
-      </select>
 
-      <label className="grid">
-      {coches.map((coche, index) => (
-        <section className="card" key={index}>
-          <img src={coche.imagenes[0]} alt={coche.nombre} />
-          <h3 className="name">{coche.nombre}</h3>
-          <p className="precio">{coche.precio_desde}</p>
-          <p className="categoria">{coche.categoria}</p>
-        </section>
-      ))}
-    </label>
     </>
   )
 }
