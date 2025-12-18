2293ee11f8dd050d25fc00d2801ada1842f6a60b
ivanroqueceb <ivan.roque.ceb@immune.institute>
2025-12-18 16:31:37 +0100

    cambios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
 import "../css/App.css"
 import coches from"../coches.json"
-import Login from "./Login"
 
 function App() {
 
   return (
     <>
-      <Login/>
+      <label for="inputTipo">Tipo: </label>
+      <select name="" id="inputTipo">
+        <option value="">Nada</option>
+        <option value="SUV">SUV</option>
+        <option value="Berlina">Berlina</option>
+        <option value="Tourin">Touring</option>
+        <option value="Gran Coupé">Gran Coupé</option>
+        <option value="Compacto">Compacto</option>
+        <option value="Monovolumen">Monovolumen</option>
+        <option value="Cabrio">Cabrio</option>
+      </select>
+
+      <label className="grid">
+      {coches.map((coche, index) => (
+        <section className="card" key={index}>
+          <img src={coche.imagenes[0]} alt={coche.nombre} />
+          <h3 className="name">{coche.nombre}</h3>
+          <p className="precio">{coche.precio_desde}</p>
+          <p className="categoria">{coche.categoria}</p>
+        </section>
+      ))}
+    </label>
     </>
   )
 }
